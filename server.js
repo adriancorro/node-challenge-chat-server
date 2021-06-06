f9835ee77776468ee02402267d0c699ff1282e51
adriancorro <adriancorrog@gmail.com>
2021-06-06 14:16:45 +0200

    desarrollando modulos

--- server.js
+++ server.js
@@ -5,16 +5,29 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.json())
+
 const welcomeMessage = {
   id: 0,
   from: "Bart",
   text: "Welcome to CYF chat system!",
 };
 
+
+
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
+
+app.post('/messages', (req, res)  =>   {
+  messages.push(req.body )
+  return res.send(messages)
+})
+
+app.get('/messages', (req, res)  =>   {
+  return res.send(messages)
+} )
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
